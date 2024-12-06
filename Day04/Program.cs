e83fc0eaea0ee13303e7d8c3ed17328271f7b088
0x1452 <56117258+0x1452@users.noreply.github.com>
2024-12-06 11:57:03 +0100

    Try to improve Day 4 Part 1 implementation and compare with better/simpler approach

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,15 +1,26 @@
 ﻿
 using Common;
 using Serilog;
+using System.Diagnostics;
 
 ILogger logger = new LoggerSetup().Logger;
 
+SolvePart1("Input/example.txt");
+SolvePart1("Input/input.txt");
+SolvePart1Sobex("Input/example.txt");
+SolvePart1Sobex("Input/input.txt");
+SolvePart2("Input/example.txt");
+SolvePart2("Input/input.txt");
 
+
+// Not sure how to feel about this one.
+// Readable but unnecessarily complicated.
 void SolvePart1(string path)
 {
     var input = File.ReadAllText(path);
     var grid = GridUtils.ParseGrid(input, c => c);
 
+    var sw = Stopwatch.StartNew();
     IEnumerable<IEnumerable<char>> sequences = new[]
     {
         GridUtils.EnumerateRows(grid),
@@ -23,11 +34,83 @@ void SolvePart1(string path)
     var sum = 0;
     foreach (var sequence in sequences)
     {
-        logger.Verbose(string.Join(" ", sequence));
         sum += CountOccurrences(sequence, keyword);
     }
 
-    logger.Information($"[Part1:{path}] {sum}");
+    sw.Stop();
+    logger.Information("[Part1:{Path}] {Sum} {ElapsedMs}ms", path, sum, sw.Elapsed.TotalMilliseconds);
+}
+
+void SolvePart1Sobex(string path)
+{
+    var input = File.ReadAllText(path);
+    var grid = GridUtils.ParseGrid(input, c => c);
+
+    var sw = Stopwatch.StartNew();
+    var rowCount = grid.GetLength(0);
+    var columnCount = grid.GetLength(1);
+
+    var keyword = "XMAS";
+
+    // Approach yoinked from SOBEX
+    var sum = 0;
+    var directions = new List<(int dy, int dx)> { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
+    for (int x = 0; x < rowCount; x++)
+    {
+        for (int y = 0; y < columnCount; y++)
+        {
+            if (grid[y, x] == keyword[0])
+            {
+                sum += CheckDirections(grid, keyword, directions, x, y);
+            }
+        }
+    }
+
+    sw.Stop();
+    logger.Information("[Part1 SOBEX:{Path}] {Sum} {ElapsedMs}ms", path, sum, sw.Elapsed.TotalMilliseconds);
+}
+
+int CheckDirections(char[,] grid, string keyword, List<(int dy, int dx)> directions, int x, int y)
+{
+    var sum = 0;
+    foreach (var (dy, dx) in directions)
+    {
+        var isValid = true;
+
+        for (int i = 1; i < keyword.Length; i++)
+        {
+            var nextX = x + dx * i;
+            var nextY = y + dy * i;
+
+            if (IsOutOfBounds(grid, nextX, nextY))
+            {
+                isValid = false;
+                break;
+            }
+
+            if (grid[nextY, nextX] != keyword[i])
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid)
+            sum++;
+    }
+
+    return sum;
+}
+
+bool IsOutOfBounds(char[,] grid, int x, int y)
+{
+    var rowCount = grid.GetLength(0);
+    var columnCount = grid.GetLength(1);
+
+    return x < 0
+        || x >= columnCount
+        || y < 0
+        || y >= rowCount;
 }
 
 void SolvePart2(string path)
@@ -35,6 +118,7 @@ void SolvePart2(string path)
     var input = File.ReadAllText(path);
     var grid = GridUtils.ParseGrid(input, c => c);
 
+    var sw = Stopwatch.StartNew();
     var rowCount = grid.GetLength(0);
     var columnCount = grid.GetLength(1);
 
@@ -62,32 +146,48 @@ void SolvePart2(string path)
         }
     }
 
-    logger.Information($"[Part2:{path}] {sum}");
+    sw.Stop(); 
+    logger.Information("[Part2:{Path}] {Sum} {ElapsedMs}ms", path, sum, sw.Elapsed.TotalMilliseconds);
 }
 
 static int CountOccurrences(IEnumerable<char> chars, string keyword)
 {
     var queue = new Queue<char>(keyword.Length);
-    var index = 0;
 
     var count = 0;
     foreach (var c in chars)
     {
         queue.Enqueue(c);
 
+
         if (queue.Count > keyword.Length)
             queue.Dequeue();
 
-        if (queue.Count == keyword.Length && (queue.SequenceEqual(keyword) || queue.SequenceEqual(keyword.Reverse())))
-            count++;
+        if (queue.Count == keyword.Length)
+        {
+            var matchesKeyword = true;
+            var matchesReversed = true;
 
-        index++;
+            var index = 0;
+
+            foreach (var item in queue)
+            {
+                if (matchesKeyword && item != keyword[index])
+                    matchesKeyword = false;
+                if (matchesReversed && item != keyword[keyword.Length - 1 - index])
+                    matchesReversed = false;
+
+                index++;
+
+                if (!matchesKeyword && !matchesReversed)
+                    break;
+            }
+
+            if (matchesKeyword || matchesReversed)
+                count++;
+        }
     }
 
     return count;
 }
 
-SolvePart1("Input/example.txt");
-SolvePart1("Input/input.txt");
-SolvePart2("Input/example.txt");
-SolvePart2("Input/input.txt");
