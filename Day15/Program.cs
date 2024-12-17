216dba6b5a753769235fb2a00c2362ac04fd9a63
0x1452 <56117258+0x1452@users.noreply.github.com>
2024-12-17 13:29:08 +0100

    Add day 15 part 2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -11,6 +11,10 @@ Dictionary<char, (int dx, int dy)> directions = new()
 SolvePart1("Input/example1.txt");
 SolvePart1("Input/example2.txt");
 SolvePart1("Input/input.txt");
+SolvePart2("Input/example1.txt");
+SolvePart2("Input/example2.txt");
+SolvePart2("Input/example3.txt");
+SolvePart2("Input/input.txt");
 
 void SolvePart1(string filepath)
 {
@@ -22,13 +26,7 @@ void SolvePart1(string filepath)
         (x, y) = Move(grid, x, y, move);
     }
 
-    Console.WriteLine(GridUtils.GridString(grid, c => c switch
-    {
-        CellType.Wall => "#",
-        CellType.Box => "O",
-        CellType.Robot => "@",
-        _ => "."
-    }));
+    PrintGrid(grid);
 
     var rows = grid.GetLength(0);
     var cols = grid.GetLength(1);
@@ -46,25 +44,59 @@ void SolvePart1(string filepath)
     Console.WriteLine($"[Part1:{filepath}] {sum}");
 }
 
+void SolvePart2(string filepath)
+{
+    var (grid, moves, x, y) = ParseInput(filepath);
+
+    grid = ExtendGrid(grid);
+    x *= 2;
+
+    foreach (var move in moves)
+    {
+        (x, y) = Move(grid, x, y, move);
+    }
+
+    PrintGrid(grid);
+
+    var rows = grid.GetLength(0);
+    var cols = grid.GetLength(1);
+
+    var sum = 0;
+    for (int ix = 0; ix < cols; ix++)
+    {
+        for (int iy = 0; iy < rows; iy++)
+        {
+            if (grid[iy, ix] == CellType.BoxLeft)
+                sum += 100 * iy + ix;
+        }
+    }
+
+    Console.WriteLine($"[Part2:{filepath}] {sum}");
+}
+
 (int x, int y) Move(CellType[,] grid, int x, int y, char direction)
 {
-    var currentCell = grid[y, x];
-    if (currentCell != CellType.Robot && currentCell != CellType.Box)
+    if (!CanMove(grid, x, y, direction))
         return (x, y);
+
+    var currentCell = grid[y, x];
 
     var (dx, dy) = directions[direction];
-    var nextIsOutOfBounds = GridUtils.IsOutOfBounds(grid, x + dx, y + dy);
-
-    if (nextIsOutOfBounds)
-        return (x, y);
-
     var nextCell = grid[y + dy, x + dx];
-
-    if (nextCell == CellType.Wall)
-        return (x, y);
 
     if (nextCell == CellType.Box)
         Move(grid, x + dx, y + dy, direction);
+
+    if (nextCell == CellType.BoxLeft || nextCell == CellType.BoxRight)
+    {
+        if (direction == '^' || direction == 'v')
+        {
+            var (otherX, otherY) = GetOtherBoxPart(grid, x + dx, y + dy);
+            Move(grid, otherX, otherY, direction);
+        }
+
+        Move(grid, x + dx, y + dy, direction);
+    }
 
     nextCell = grid[y + dy, x + dx];
 
@@ -79,10 +111,56 @@ void SolvePart1(string filepath)
     return (x, y);
 }
 
+(int otherX, int otherY) GetOtherBoxPart(CellType[,] grid, int x, int y)
+{
+    var (dx, dy) = grid[y, x] == CellType.BoxLeft
+        ? directions['>']
+        : directions['<'];
+
+    return (x + dx, y + dy);
+}
+
+bool CanMove(CellType[,] grid, int x, int y, char direction)
+{
+    if (GridUtils.IsOutOfBounds(grid, x, y))
+        return false;
+
+    var currentCell = grid[y, x];
+    if (!IsMovable(currentCell))
+        return false;
+
+    var (dx, dy) = directions[direction];
+    if (GridUtils.IsOutOfBounds(grid, x + dx, y + dy))
+        return false;
+
+    var nextCell = grid[y + dy, x + dx];
+
+    if (nextCell == CellType.Wall)
+        return false;
+
+    if (nextCell == CellType.Box)
+        return CanMove(grid, x + dx, y + dy, direction);
+
+    if (nextCell == CellType.BoxLeft || nextCell == CellType.BoxRight)
+    {
+        if (direction == '^' || direction == 'v')
+        {
+            var (otherX, otherY) = GetOtherBoxPart(grid, x + dx, y + dy);
+            if (!CanMove(grid, otherX, otherY, direction))
+                return false;
+        }
+
+        if (!CanMove(grid, x + dx, y + dy, direction))
+            return false;
+    }
+
+    return true;
+}
+
 static Input ParseInput(string filepath)
 {
-    var content = File.ReadAllText(filepath).Split("\n\n");
-    var lines = content[0].Split('\n', StringSplitOptions.RemoveEmptyEntries).Select(line => line.Trim()).ToList();
+    var content = File.ReadAllText(filepath).Split(["\n\n", "\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
+    var lines = content[0].Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries).Select(line => line.Trim()).ToList();
     int rows = lines.Count;
     int cols = lines[0].Length;
     var grid = new CellType[rows, cols];
@@ -110,8 +188,63 @@ static Input ParseInput(string filepath)
         }
     }
 
-    var moves = content[1].Replace("\n", "");
+    var moves = content[1].Replace("\n", "").Replace("\r", "");
     return new Input(grid, moves.ToCharArray(), startX, startY);
+}
+
+static CellType[,] ExtendGrid(CellType[,] grid)
+{
+    var rows = grid.GetLength(0);
+    var cols = grid.GetLength(1);
+
+    var extendedGrid = new CellType[rows, cols * 2];
+
+    for (int y = 0; y < rows; y++)
+    {
+        for (int x = 0; x < cols; x++)
+        {
+            var (first, second) = grid[y, x] switch
+            {
+                CellType.Wall => (CellType.Wall, CellType.Wall),
+                CellType.Box => (CellType.BoxLeft, CellType.BoxRight),
+                CellType.Robot => (CellType.Robot, CellType.Empty),
+                _ => (CellType.Empty, CellType.Empty)
+            };
+
+            extendedGrid[y, x * 2] = first;
+            extendedGrid[y, x * 2 + 1] = second;
+        }
+    }
+
+    return extendedGrid;
+}
+
+static bool IsMovable(CellType cellType)
+{
+    switch (cellType)
+    {
+        case CellType.Robot:
+        case CellType.Box:
+        case CellType.BoxLeft:
+        case CellType.BoxRight:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+static void PrintGrid(CellType[,] grid)
+{
+    Console.WriteLine(GridUtils.GridString(grid, c => c switch
+    {
+        CellType.Wall => "#",
+        CellType.Box => "O",
+        CellType.BoxLeft => "[",
+        CellType.BoxRight => "]",
+        CellType.Robot => "@",
+        _ => "."
+    }));
 }
 
 enum CellType
@@ -119,6 +252,8 @@ enum CellType
     Empty,
     Wall,
     Box,
+    BoxLeft,
+    BoxRight,
     Robot
 }
 
