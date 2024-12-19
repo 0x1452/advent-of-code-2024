5086e53230e15f56ba4f35190a87aa79724d6e38
0x1452 <56117258+0x1452@users.noreply.github.com>
2024-12-19 21:20:16 +0100

    Add day 19 part 2

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,12 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
 using Common;
 using Serilog;
 
 ILogger logger = new LoggerSetup().Logger;
+Dictionary<(string design, int completedLength), long> matchCache = [];
+
 SolvePart1("Input/example.txt");
 SolvePart1("Input/input.txt");
 
+SolvePart2("Input/example.txt");
+SolvePart2("Input/input.txt");
 
 void SolvePart1(string filepath)
 {
@@ -25,8 +29,26 @@ void SolvePart1(string filepath)
     logger.Information($"[Part1:{filepath}] Possible designs: {possibleCount}");
 }
 
+void SolvePart2(string filepath)
+{
+    var (patterns, designs) = ParseInput(filepath);
+    matchCache.Clear();
 
-bool FindMatch(ImmutableSortedDictionary<int, List<string>> patterns, string design, int completedLength, List<string> patternsUsed)
+    var sw = Stopwatch.StartNew();
+    long possibleCount = 0;
+    foreach (var design in designs)
+    {
+        var count = FindMatches(patterns, design, completedLength: 0);
+        possibleCount += count;
+
+        logger.Debug($"{design}: {count}");
+    }
+    sw.Stop();
+
+    logger.Information($"[Part2:{filepath}] Possible arrangements: {possibleCount}  {sw.Elapsed.TotalMilliseconds}ms");
+}
+
+bool FindMatch(ImmutableSortedDictionary<int, HashSet<string>> patterns, string design, int completedLength, List<string> patternsUsed)
 {
     logger.Verbose($"Called FindMatch: {design}, {completedLength}, Patterns used: ({string.Join(", ", patternsUsed)})");
     var foundMatch = false;
@@ -38,17 +60,14 @@ bool FindMatch(ImmutableSortedDictionary<int, List<string>> patterns, string des
 
         var region = design[completedLength..(completedLength + length)];
 
-        foreach (var pattern in patterns[length])
+        if (patterns[length].Contains(region))
         {
-            if (region == pattern)
+            if (FindMatch(patterns, design, completedLength + length, patternsUsed))
             {
-                if (FindMatch(patterns, design, completedLength + length, patternsUsed))
-                {
-                    patternsUsed.Add(pattern);
-                    completedLength += length;
-                    foundMatch = true;
-                    break;
-                }
+                patternsUsed.Add(region);
+                completedLength += length;
+                foundMatch = true;
+                break;
             }
         }
 
@@ -62,6 +81,42 @@ bool FindMatch(ImmutableSortedDictionary<int, List<string>> patterns, string des
     return foundMatch;
 }
 
+
+long FindMatches(ImmutableSortedDictionary<int, HashSet<string>> patterns, string design, int completedLength)
+{
+    logger.Verbose($"Called FindMatches: {design}, {completedLength}");
+
+    if (design.Length == completedLength)
+        return 1;
+
+    if (matchCache.TryGetValue((design, completedLength), out var cachedFound))
+        return cachedFound;
+
+    long totalFound = 0;
+
+    var start = Math.Min(design.Length - completedLength, patterns.Keys.First());
+    for (int length = start; length >= 1; length--)
+    {
+        var region = design[completedLength..(completedLength + length)];
+
+        if (patterns[length].Contains(region))
+        {
+            logger.Verbose(design[..(completedLength + length)]);
+            var found = FindMatches(patterns, design, completedLength + length);
+
+            matchCache[(design, completedLength + length)] = found;
+
+            if (found > 0)
+            {
+                totalFound += found;
+            }
+        }
+    }
+
+    return totalFound;
+}
+
+
 Input ParseInput(string filepath)
 {
     var content = File.ReadAllLines(filepath);
@@ -71,7 +126,7 @@ Input ParseInput(string filepath)
         .GroupBy(s => s.Length)
         .ToImmutableSortedDictionary(
             g => g.Key,
-            g => g.ToList(),
+            g => g.ToHashSet(),
             Comparer<int>.Create((a, b) => b.CompareTo(a)));
 
     var designs = new List<string>();
@@ -83,4 +138,4 @@ Input ParseInput(string filepath)
 
     return new Input(patterns, designs);
 }
-record Input(ImmutableSortedDictionary<int, List<string>> Patterns, List<string> Designs);
+record Input(ImmutableSortedDictionary<int, HashSet<string>> Patterns, List<string> Designs);
